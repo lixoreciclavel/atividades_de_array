34006296548b12f57fa016143f9e7b12e6dc1ddb
Levy Cordeiro <108874182+lixoreciclavel@users.noreply.github.com>
2023-04-10 12:23:21 -0300

    Add files via upload

---
+++ atividades 10-04-2023/exemplo.js
@@ -0,0 +1,63 @@
+//Método push//
+
+//const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+
+//users.push("Levy", "Kevin", "Toinho");
+
+//console.log(users);
+
+
+//Método pop//
+
+
+//const person = ["Felipe", 30, "São Paulo", "Masculino", "222333232303"]
+
+//person.pop();
+//person.pop();
+
+//console.log(person);
+
+
+//Método shift//
+
+
+//const fruits = ["Apple", "Orange", "Banana", "Pear"];
+
+//fruits.shift();
+
+//console.log(fruits);
+
+
+//Método unshift//
+
+//const jobs = ["FrontEnd", "BackEnd", "DevOps", "FullStack"];
+
+//jobs.unshift("Design");
+
+//console.log(jobs);
+
+
+//Método IndexOf//
+
+//const jobs = ["FrontEnd", "BackEnd", "DevOps", "FullStack"];
+//let indexOfSearch = jobs.indexOf("DevOps", 2);
+
+//console.log(indexOfSearch);
+
+
+// Método sort//
+
+//const jobs = [1, 2, 3, 5, 6, 7, 1, 9];
+
+//jobs.sort();
+
+//console.log(jobs);
+
+
+//Método Join//
+
+const color = ["Red", "Green", "White", "Black"];
+
+let join = color.join(" --> ");
+
+console.log(join);
